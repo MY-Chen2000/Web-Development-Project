9105e1b604f709d4afcbdcf994389eaf8e96aaaa
MY-Chen2000 <mic016@ucsd.edu>
2021-12-25 00:58:18 -0800

    12/25/21

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express=require('express');
-const app=express();
 const path = require('path');
+const mongoose = require('mongoose');
+const campground = require ('./models/campground');
+mongoose.connect('mongodb://localhost:27017/camp')
+const db = mongoose.connection;
+db.on('error',console.error.bind(console,"connection error:"),{
+    useNewUrlParser:true
+});
+db.once('open',()=>{
+    console.log("Connected")
+});
+
+const app=express();
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
@@ -8,8 +19,15 @@ app.set('views',path.join(__dirname,'views'));
 app.get('/',(req,res) => {
     res.render('home');
 })
+app.get('/campground',async (req,res) => {
+    const camps = await campground.find({});
+    res.render('campgrounds/index',{camps});
+})
+app.get('/campground/:id',async (req,res) => {
+    const camp = await campground.findById(req.params.id);
+    res.render('campgrounds/show',{camp});
+})
 
 app.listen(3000,() =>{
     console.log("Serving Port: 3000");
 })
-
---
+++ seed/index.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const campground = require ('../models/campground');
+mongoose.connect('mongodb://localhost:27017/camp')
+const db = mongoose.connection;
+db.on('error',console.error.bind(console,"connection error:"),{
+    useNewUrlParser:true
+});
+db.once('open',()=>{
+    console.log("Connected")
+});
+
+
+const cities=require('./cities');
+const {descriptors, places} = require('./helper');
+
+const randomElement = (array) => array[Math.floor(Math.random()*array.length)];
+
+const seedDB = async () => {
+    await campground.deleteMany({});
+    console.log("deleted");
+    for (let i = 0; i<50; i++){
+        const sampleCity = randomElement(cities);
+        const camp=new campground({      
+            location:  `${sampleCity.city}, ${sampleCity.state}`,
+            title: `${randomElement(descriptors)} ${randomElement(places)}`
+        });
+        await camp.save();       
+    }
+    console.log("saved");
+}
+seedDB().then(() => {
+    mongoose.connection.close();
+});
